13001d06c4508e47c6f817023153600b656ee710
mirunascoban <miruna.scoban@gmail.com>
2021-12-16 19:08:21 +0200

    Second commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+//Array Left rotation
+$array = [1, 2, 3, 4, 5];
+$n = 3;
+
+for($i = 0; $i < $n; $i++){
+    $first = $array[0];
+    for($j = 0; $j < count($array)-1; $j++){
+        $array[$j] = $array[$j+1];
+    }
+    $array[$j] = $first;
+}
+
+for ($i = 0; $i < count($array); $i++) {
+    print($array[$i] . " ");
+}
+
+/****************************************************************** */
+
+//Find certain item in array
+$array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+$count = 0;
+
+foreach ($array as $item) {
+  if ($item === 1) {
+    $count++;
+    break;
+  }
+}
+
+print_r($count);
+
+/****************************************************************** */
+
+//Array item occurency
+$array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+$count = [];
+
+foreach ($array as $value) {
+  if (isset($count[$value])) {
+    $count[$value] = $count[$value] + 1;
+  } else {
+    $count[$value] = 1;
+  }
+}
+
+print_r($count);
+
+/****************************************************************** */
+
+// Reverse array
+$array = [1, 2, 3, 4, 5];
+
+$size = sizeof($array);
+
+for($i = $size-1; $i >= 0; $i--){
+    echo $array[$i];
+}
+
+?>
